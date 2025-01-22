a2d355218adc774280cf952e1c8f60395240f3e0
kodi3121 <pavansri2222@gmail.com>
2025-01-22 07:42:30 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,1 @@
-import json
-
-def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!....... (Modified v0.2')
-    }
-
+print("Hi! This is a lambda function")
